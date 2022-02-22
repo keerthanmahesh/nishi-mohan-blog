7cd3d54eb12f91f564f555bb233a57f00ca106f4
Keerthan Mahesh <keerthan02@gmail.com>
2022-02-23 02:56:50 +0530

    landing page

---
+++ src/components/landing.js
@@ -0,0 +1,11 @@
+export const Landing = (props) => {
+  return (
+    <div className='landing'>
+      <div className='intro'>
+        <h1 className='header'>
+          just a 20-something year old exploring the world one step at a time
+        </h1>
+      </div>
+    </div>
+  )
+}
